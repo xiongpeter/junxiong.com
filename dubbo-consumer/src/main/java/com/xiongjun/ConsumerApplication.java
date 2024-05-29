0236462c24b81e51d318d46e4fe74fbff4b92af7
jun.xiong <245641147@qq.com>
2024-05-29 12:54:39 +0800

    修改

--- dubbo-consumer/src/main/java/com/xiongjun/ConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/xiongjun/ConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CosumerApplication {
+public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CosumerApplication.class);
+        SpringApplication.run(ConsumerApplication.class);
     }
 
 }
